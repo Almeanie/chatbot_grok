f3f3cd5cef7420c8d10836ce820baef802d4d51b
Almeanie <93413927+Almeanie@users.noreply.github.com>
2025-03-07 02:00:14 +0100

    functional grok ai chatbot

---
+++ components/grok-chat.tsx
@@ -0,0 +1,43 @@
+"use client"
+ 
+import { useChat, type UseChatOptions } from "ai/react"
+ 
+import { Chat } from "@/components/ui/chat"
+ 
+type ChatDemoProps = {
+  initialMessages?: UseChatOptions["initialMessages"]
+}
+ 
+export function GrokChat(props: ChatDemoProps) {
+  const {
+    messages,
+    input,
+    handleInputChange,
+    handleSubmit,
+    append,
+    stop,
+    isLoading,
+    setMessages,
+  } = useChat(props)
+ 
+  return (
+    <div className="flex h-[500px] w-full">
+      <Chat
+        className="grow"
+        messages={messages}
+        handleSubmit={handleSubmit}
+        input={input}
+        handleInputChange={handleInputChange}
+        isGenerating={isLoading}
+        stop={stop}
+        append={append}
+        setMessages={setMessages}
+        suggestions={[
+          "Generate a tasty vegan lasagna recipe for 3 people.",
+          "Generate a list of 5 questions for a job interview for a software engineer.",
+          "Who won the 2022 FIFA World Cup?",
+        ]}
+      />
+    </div>
+  )
+}
